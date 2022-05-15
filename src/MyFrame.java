7417dfc6dad1e9296a0de9571d34f34d99feed6f
DantheOPMan <soccerdan34@gmail.com>
2022-05-15 00:23:10 -0400

    now works fullscreen opaque

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,17 +1,16 @@
-import java.awt.Color;
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class MyFrame extends JFrame{
     MyPanel panel;
     
     MyFrame(){
         panel = new MyPanel();
-
+        Toolkit tk = Toolkit.getDefaultToolkit();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //this.setSize(1920,1080);
-        //this.setUndecorated(true);
-        //this.getContentPane().setBackground(new Color(1.0f,1.0f,1.0f,0.0f));
-        //this.setBackground(new Color(1.0f,1.0f,1.0f,0.0f));
+        this.setSize((int)tk.getScreenSize().getWidth(),(int)tk.getScreenSize().getHeight());
+        this.setUndecorated(true);
+        this.setBackground(new Color(1.0f,1.0f,1.0f,0.0f));
         this.setAlwaysOnTop(true);
         this.add(panel);
         this.getRootPane().putClientProperty("apple.awt.draggableWindowBackground",true);
@@ -20,26 +19,3 @@ public class MyFrame extends JFrame{
         this.setVisible(true);
     }
 }
-// general code stuff will look at to use later
-/*
- * dan.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
- * dan.setSize(400,500);
- * dan.setLocation(200,200);
- * dan.setUndecorated(true);
- * dan.setVisible(true);
- * 
- * JFrame frame = new JFrame("Transparent Window");
- * frame.setUndecorated(true);
- * frame.setBackground(new Color(0, 0, 0, 255));
- * frame.setAlwaysOnTop(true);
- * // Without this, the window is draggable from any non transparent
- * // point, including points inside textboxes.
- * frame.getRootPane().putClientProperty("apple.awt.draggableWindowBackground",true);
- * frame.getContentPane().setLayout(new java.awt.BorderLayout());
- * frame.getContentPane().add(new JTextField("text field north"),
- * java.awt.BorderLayout.NORTH);
- * frame.getContentPane().add(new JTextField("text field south"),
- * java.awt.BorderLayout.SOUTH);
- * frame.setVisible(true);
- * frame.pack();
- */
--- src/MyPanel.java
+++ src/MyPanel.java
@@ -3,23 +3,24 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class MyPanel extends JPanel implements ActionListener{
-
-    final int PANEL_WIDTH = 500;
-    final int PANEL_HEIGHT = 500;
+    final int PANEL_WIDTH = 1920;
+    final int PANEL_HEIGHT = 1080;
     Image phoenix;
     int phoenixWidth = 100;
     int phoenixHeight = 100;
     Image backgroundImage;
     Timer timer;
     int time = 10;
-    int xVelocity = 2;
-    int yVelocity = 1;
+    int xVelocity = 4;
+    int yVelocity = 4;
     int x = 0;
     int y = 0;
 
     MyPanel(){
         this.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
-        this.setBackground(Color.black);
+        this.setOpaque(false);
+
+        //this.setBackground(Color.black);
         phoenix = new ImageIcon("Phoenix.png").getImage().getScaledInstance(phoenixWidth, phoenixHeight,Image.SCALE_DEFAULT);
         timer = new Timer(time, this);
         timer.start();
@@ -28,19 +29,20 @@ public class MyPanel extends JPanel implements ActionListener{
     public void paint(Graphics g){
         super.paint(g); 
         Graphics2D g2D  = (Graphics2D) g;
-        g2D.drawImage(phoenix, x, y, null); 
+       g2D.drawImage(phoenix, x, y, null); 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        
         if(x >= PANEL_WIDTH-phoenix.getWidth(null) || x <0){
             xVelocity = xVelocity * -1;
         }
-        if(y >= PANEL_WIDTH-phoenix.getHeight(null) || y <0){
+        if(y >= PANEL_HEIGHT-phoenix.getHeight(null) || y < 0){
             yVelocity = yVelocity * -1;
         }
-        x = x +xVelocity;
-        y= y+yVelocity;
+        x = x + xVelocity;
+        y = y + yVelocity;
         repaint();
         
     }
